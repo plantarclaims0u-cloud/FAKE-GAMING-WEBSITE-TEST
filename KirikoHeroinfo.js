ffcbb7b09800aa12638270ce82f284f7a3b2561f
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2025-11-19 08:43:58 -0600

    Add files via upload

---
+++ KirikoHeroinfo.js
@@ -0,0 +1,23 @@
+//KirikoHeroinfo .js file
+
+//console.log("what you talking about willis");
+
+window.onload = function(){
+	
+	let popup = document.getElementById("pop-up");
+	popup.addEventListener("click", myFunction);
+
+
+	function myFunction(){
+		popup.style.visibility = "hidden";
+	}	
+
+let popup = document.getElementById("pop-up");
+	popup.addEventListener("click", myFunction);
+
+
+	function myFunction(){
+		popup.style.visibility = "visable";
+	}	
+}
+
