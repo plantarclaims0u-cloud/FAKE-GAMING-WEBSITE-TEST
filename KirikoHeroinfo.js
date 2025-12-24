d2f6b2761f03f163d56000cf448a326e1221da50
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2025-12-24 16:56:40 -0600

    Add toggle function for displaying abilities

--- KirikoHeroinfo.js
+++ KirikoHeroinfo.js
@@ -1,7 +1,18 @@
 //KirikoHeroinfo .js file
 
-  function showAbilities() {
+
+  function toggleAbilities() {
     var abilities = document.getElementById("kirikoAbilities");
-    abilities.style.display = "block";
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Kiriko’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Kiriko’s Abilities";
+    }
   }
+
+
 
