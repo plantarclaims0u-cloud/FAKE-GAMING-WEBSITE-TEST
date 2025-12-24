f85e4d30d4b84f44fb5583b794bef16c5a7ce7b4
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2025-12-24 16:35:08 -0600

    Remove unnecessary script tags from KirikoHeroinfo.js

--- KirikoHeroinfo.js
+++ KirikoHeroinfo.js
@@ -1,8 +1,6 @@
 //KirikoHeroinfo .js file
 
-<script>
   function showAbilities() {
     var abilities = document.getElementById("Kiriko-Abilities");
     abilities.style.display = "block";
   }
-</script>
