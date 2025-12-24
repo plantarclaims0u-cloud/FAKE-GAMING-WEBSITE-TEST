ae40bb06209d4433831a53b7172c02a95c9bec36
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2025-12-24 16:28:43 -0600

    Add script to display Kiriko abilities
    Added script to show abilities in KirikoHeroinfo.js

--- KirikoHeroinfo.js
+++ KirikoHeroinfo.js
@@ -1,6 +1,8 @@
 //KirikoHeroinfo .js file
 
-
-
-
-
+<script>
+  function showAbilities() {
+    var abilities = document.getElementById("Kiriko-Abilities");
+    abilities.style.display = "block";
+  }
+</script>
