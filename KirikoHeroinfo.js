b9e1ca60d0791b15867305ebadf701d4eb100a10
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2025-12-24 11:42:31 -0600

    Refactor popup event listener and visibility logic
    Removed duplicate event listener and corrected visibility setting.

--- KirikoHeroinfo.js
+++ KirikoHeroinfo.js
@@ -1,23 +1,4 @@
 //KirikoHeroinfo .js file
 
-//console.log("what you talking about willis");
 
-window.onload = function(){
-	
-	let popup = document.getElementById("pop-up");
-	popup.addEventListener("click", myFunction);
-
-
-	function myFunction(){
-		popup.style.visibility = "hidden";
-	}	
-
-let popup = document.getElementById("pop-up");
-	popup.addEventListener("click", myFunction);
-
-
-	function myFunction(){
-		popup.style.visibility = "visable";
-	}	
-}
 
