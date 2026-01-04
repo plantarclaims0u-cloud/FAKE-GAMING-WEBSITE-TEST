a1afa48a29f219c86b2201e79c246f34efe4f7b3
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-01-04 13:55:57 -0600

    Update button text in toggleAbilities function

--- KirikoHeroinfo.js
+++ KirikoHeroinfo.js
@@ -7,12 +7,13 @@
 
     if (abilities.style.display === "none" || abilities.style.display === "") {
       abilities.style.display = "flex";
-      button.innerHTML = "Hide Kiriko’s Abilities";
+      button.innerHTML = "my farts stink";
     } else {
       abilities.style.display = "none";
       button.innerHTML = "Show Kiriko’s Abilities";
     }
   }
+
 
 
 
