9bdeb2d8e00a93d3c043494212fd106e66f01103
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 11:53:00 -0500

    Create JunoHeroinfo.js

---
+++ JunoHeroinfo.js
@@ -0,0 +1,15 @@
+//KirikoHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("junoAbilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Juno’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Juno’s Abilities";
+    }
+  }
