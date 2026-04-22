07a0f00305cde0f4ae19db64e49e1d46621f4c2c
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 22:07:37 -0500

    Create MeiHeroinfo.js

---
+++ MeiHeroinfo.js
@@ -0,0 +1,15 @@
+//MeiHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("Mei-Abilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Mei’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Mei’s Abilities";
+    }
+  }
