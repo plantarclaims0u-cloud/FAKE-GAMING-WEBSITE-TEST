83ca3463b2f449a4eca85106b34bead7cfc9dc6f
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 19:15:22 -0500

    Create JunkerQueenHeroinfo.js

---
+++ JunkerQueenHeroinfo.js
@@ -0,0 +1,15 @@
+//JunkerQueenHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("JunkerQueen-Abilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide JunkerQueen’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show JunkerQueen’s Abilities";
+    }
+  }
