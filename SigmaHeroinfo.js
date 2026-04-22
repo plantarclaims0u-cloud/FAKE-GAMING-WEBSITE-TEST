a7e4d04c4749cec96f50136eae3221a48fbcc1f4
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 19:35:42 -0500

    Create SigmaHeroinfo.js

---
+++ SigmaHeroinfo.js
@@ -0,0 +1,15 @@
+//SigmaHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("Sigma-Abilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Sigma’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Sigma’s Abilities";
+    }
+  }
