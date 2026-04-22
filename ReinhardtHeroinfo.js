819fbf840568857f1de80245dda1a7b3da06490a
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 19:25:52 -0500

    Create ReinhardtHeroinfo.js

---
+++ ReinhardtHeroinfo.js
@@ -0,0 +1,14 @@
+//DvaHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("Reinhardt-Abilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Reinhardt’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Reinhardt’s Abilities";
+    }
