ddc4a28348b6aa7c7c6ddb7538f1b72b7c81a9e4
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-04-21 21:57:45 -0500

    Create SoujornHeroinfo.js

---
+++ SoujornHeroinfo.js
@@ -0,0 +1,15 @@
+//SoujornHeroinfo .js file
+
+
+  function toggleAbilities() {
+    var abilities = document.getElementById("Soujorn-Abilities");
+    var button = document.getElementById("showButton");
+
+    if (abilities.style.display === "none" || abilities.style.display === "") {
+      abilities.style.display = "flex";
+      button.innerHTML = "Hide Soujorn’s Abilities";
+    } else {
+      abilities.style.display = "none";
+      button.innerHTML = "Show Soujorn’s Abilities";
+    }
+  }
