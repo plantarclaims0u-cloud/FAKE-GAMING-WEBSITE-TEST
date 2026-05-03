7f2fa03fe103f1c4b26318cd5bb20536012781b9
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-05-03 15:04:14 -0500

    Refactor jsmenu to use an unordered list
    Updated the menu structure to use an unordered list with multiple items.

--- testinclude.js
+++ testinclude.js
@@ -1,4 +1,11 @@
 
 window.onload = function() {
- //document.getElementById("jsmenu").innerHTML = "<a href='#1'>menu1</a> menu2 menu3 menu4";
+ document.getElementById("jsmenu").innerHTML = "<ul>
+				<li><a href=''>Item 1</a></li>
+				<li><a href=''>Item 2</a></li>
+				<li><a href=''>Item 3</a></li>
+				<li><a href=''>Item 4</a></li>
+				<li><a href=''>Item 5</a></li>
+
+</ul>";
 };
