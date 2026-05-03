2a0e915cca32e1590d3a7c0450eea6b8e06c87df
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-05-03 14:57:34 -0500

    Comment out jsmenu innerHTML assignment
    Comment out the line that sets the innerHTML of jsmenu.

--- testinclude.js
+++ testinclude.js
@@ -1,4 +1,4 @@
 
 window.onload = function() {
- document.getElementById("jsmenu").innerHTML = "<a href='#1'>menu1</a> menu2 menu3 menu4";
+ //document.getElementById("jsmenu").innerHTML = "<a href='#1'>menu1</a> menu2 menu3 menu4";
 };
