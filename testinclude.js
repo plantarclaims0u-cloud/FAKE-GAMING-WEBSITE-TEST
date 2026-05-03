a114d58f5d7a33d3d9058650c39811bb82daf26b
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-05-03 15:09:06 -0500

    Update testinclude.js

--- testinclude.js
+++ testinclude.js
@@ -1,11 +1,4 @@
 
 window.onload = function() {
- document.getElementById("jsmenu").innerHTML = "<ul>
-				<li><a href=''>Item 1js</a></li>
-				<li><a href=''>Item 2</a></li>
-				<li><a href=''>Item 3</a></li>
-				<li><a href=''>Item 4</a></li>
-				<li><a href=''>Item 5</a></li>
-
-</ul>";
+ document.getElementById("jsmenu").innerHTML = "<ul><li><a href=''>Item 1js</a></li><li><a href=''>Item 2</a></li><li><a href=''>Item 3</a></li><li><a href=''>Item 4</a></li><li><a href=''>Item 5</a></li></ul>";
 };
