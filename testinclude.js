d3ba715e2b786c872a6f94d072913f46f40293cd
plantarclaims0u-cloud <plantar.claims_0u@icloud.com>
2026-05-03 15:05:29 -0500

    Update testinclude.js

--- testinclude.js
+++ testinclude.js
@@ -1,7 +1,7 @@
 
 window.onload = function() {
  document.getElementById("jsmenu").innerHTML = "<ul>
-				<li><a href=''>Item 1</a></li>
+				<li><a href=''>Item 1js</a></li>
 				<li><a href=''>Item 2</a></li>
 				<li><a href=''>Item 3</a></li>
 				<li><a href=''>Item 4</a></li>
